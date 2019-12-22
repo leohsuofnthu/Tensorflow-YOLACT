98fd1604fe590c76bd13366325c8ed81a40b4f2b
leohsuofnthu <leohsuofnthu@gmail.com>
2019-12-22 16:44:02 -0500

    initial project architecture

---
+++ models/yolact.py
@@ -0,0 +1,28 @@
+"""
+YOLACT:Real-time Instance Segmentation
+Ref: https://arxiv.org/abs/1904.02689
+
+Arthor: HSU, CHIHCHAO
+"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow as tf
+assert tf.__version__.startswith('2')
+
+
+class Yolcat(tf.keras.Model):
+    """
+        Creating the YOLCAT Architecture
+        Arguments:
+
+    """
+
+    def __init__(self):
+        pass
+
+    def call(self, inputs, training=True):
+        #
+        pass
+
