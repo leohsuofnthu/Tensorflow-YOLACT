3e973a8ea0a8a439b64a53e0464f929845daf708
leohsuofnthu <leohsuofnthu@gmail.com>
2020-01-04 21:54:57 -0500

    download the train/val images and annotations

--- data/dataset_coco.py
+++ data/dataset_coco.py
@@ -2,23 +2,41 @@ import os
 import tensorflow as tf
 
 """
+    Pre-Processing COCO 2017 from scratch.
+    Download => process label and image file path => Pre-process and img load function => make tf dataset 
     ref: https://www.tensorflow.org/tutorials/text/image_captioning
 """
 
-# Todo: Download and preprocess coco 2017 train/val
+# Todo: Download and pre-process coco 2017 train/val
 
-annotation_zip = tf.keras.utils.get_file('annotation.zip',
+# Download annotation file (annotation contains train/val)
+annotation_zip = tf.keras.utils.get_file("annotation.zip",
                                          cache_subdir=os.path.abspath('.'),
-                                         origin='http://images.cocodataset.org/annotations/annotations_trainval2017.zip',
+                                         origin="http://images.cocodataset.org/annotations/annotations_trainval2017.zip",
                                          extract=True)
-annotation_file = os.path.dirname(annotation_zip) + '/annotations/instances_train2017.json'
 
-name_of_zip = 'train2017.zip'
-if not os.path.exists(os.path.abspath('.') + '/' + name_of_zip):
-    image_zip = tf.keras.utils.get_file(name_of_zip,
+annotation_train = os.path.dirname(annotation_zip) + "/annotations/instances_train2017.json"
+annotation_valid = os.path.dirname(annotation_zip) + "/annotations/instances_val2017.json"
+
+train_zip = "train2017.zip"
+val_zip = "val2017.zip"
+
+# Download train images
+if not os.path.exists(os.path.abspath('.') + '/' + train_zip):
+    image_zip = tf.keras.utils.get_file(train_zip,
                                         cache_subdir=os.path.abspath('.'),
                                         origin='http://images.cocodataset.org/zips/train2017.zip',
                                         extract=True)
-    PATH = os.path.dirname(image_zip) + '/train2017/'
+    PATH_train = os.path.dirname(image_zip) + '/train2017/'
 else:
-    PATH = os.path.abspath('.') + '/train2017/'
+    PATH_train = os.path.abspath('.') + '/train2017/'
+
+# Download validation images
+if not os.path.exists(os.path.abspath('.') + '/' + val_zip):
+    image_zip = tf.keras.utils.get_file(val_zip,
+                                        cache_subdir=os.path.abspath('.'),
+                                        origin='http://images.cocodataset.org/zips/val2017.zip',
+                                        extract=True)
+    PATH_valid = os.path.dirname(image_zip) + '/val2017/'
+else:
+    PATH_valid = os.path.abspath('.') + '/val2017/'
