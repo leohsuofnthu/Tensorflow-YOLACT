5f61ff46f9411e7d999b1a06fbc93f542306aa7c
leohsuofnthu <leohsuofnthu@gmail.com>
2020-01-08 16:40:57 -0500

    turn coco into TFrecord shard

--- data/dataset_coco.py
+++ data/dataset_coco.py
@@ -1,9 +1,4 @@
-import tensorflow_datasets as tfds
-tfds.load('coco/2017', data_dir=".")
-
 """
-    Pre-Processing COCO 2017 from scratch.
-    Download => process label and image file path => Pre-process and img load function => make tf dataset 
-    ref: https://www.tensorflow.org/tutorials/text/image_captioning
+Read the CoCo Dataset in form of TFRecord
+Create tensorflow dataset and do the augmentation
 """
-
