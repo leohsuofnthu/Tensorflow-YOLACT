07f79eaef5b640ffd6baaa066867307f536c794c
leohsuofnthu <leohsuofnthu@gmail.com>
2020-02-15 19:36:21 -0500

    remove print

--- utils/create_prior.py
+++ utils/create_prior.py
@@ -15,7 +15,7 @@ def make_priors(img_size, feature_map_size, aspect_ratio, scale):
     prior_boxes = []
     num_anchors = []
     for idx, f_size in enumerate(feature_map_size):
-        print("Create priors for f_size:%s", f_size)
+        # print("Create priors for f_size:%s", f_size)
         count_anchor = 0
         for j, i in product(range(f_size), range(f_size)):
             f_k = img_size / (f_size + 1)
@@ -28,6 +28,6 @@ def make_priors(img_size, feature_map_size, aspect_ratio, scale):
                 prior_boxes += [x-(w/2), y-(h/2), x+(w/2), y+(h/2)]
                 count_anchor += 1
         num_anchors.append(count_anchor)
-        print(f_size, count_anchor)
+        # print(f_size, count_anchor)
     output = tf.reshape(tf.convert_to_tensor(prior_boxes), [-1, 4])
     return num_anchors, output
