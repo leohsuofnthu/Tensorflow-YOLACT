7f39e469b1184352c84b5b785f479faae8fca6f6
leohsuofnthu <leohsuofnthu@gmail.com>
2020-02-22 12:10:02 -0500

    only train image need augmentation

--- test/test_make_prior.py
+++
@@ -1,9 +0,0 @@
-from utils.create_prior import make_priors
-
-img_size = 550
-f_size = [69, 35, 18, 9, 5]
-aspect_ratio = [1, 0.5, 2]
-scales = [24, 48, 96, 192, 384]
-
-_, box = make_priors(img_size, f_size, aspect_ratio, scales)
-print(box)
