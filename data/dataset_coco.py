b04035f3bacb3f1b321076315c0c13d155f20ba2
leohsuofnthu <leohsuofnthu@gmail.com>
2020-03-18 00:18:35 -0400

    add repeat

--- data/dataset_coco.py
+++ data/dataset_coco.py
@@ -34,6 +34,7 @@ def prepare_dataloader(tfrecord_dir, batch_size, subset="train"):
 
     dataset = dataset.shuffle(buffer_size=2048)
     dataset = dataset.map(map_func=parser, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+    dataset = dataset.repeat()
     dataset = dataset.batch(batch_size)
     dataset = dataset.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
 
