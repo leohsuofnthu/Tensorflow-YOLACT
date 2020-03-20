re assure randomness of dataloader, fix normalization for valid set

data/dataset_coco.py
@@ -26,14 +26,16 @@ def prepare_dataloader(tfrecord_dir, batch_size, subset="train"):
                                   match_threshold=0.5,
                                   unmatched_threshold=0.5,
                                   mode=subset)
-
-    dataset = tf.data.Dataset.list_files(os.path.join(tfrecord_dir, "coco_%s.*" % subset)).shuffle(100)
-    dataset = dataset.interleave(tf.data.TFRecordDataset,
-                                 cycle_length=tf.data.experimental.AUTOTUNE,
-                                 num_parallel_calls=tf.data.experimental.AUTOTUNE)
+    files = tf.io.matching_files(os.path.join(tfrecord_dir, "coco_%s.*" % subset))
+    num_shards = tf.cast(tf.shape(files)[0], tf.int64)
+    shards = tf.data.Dataset.from_tensor_slices(files)
+    shards = shards.shuffle(num_shards)
+    shards = shards.repeat()
+    dataset = shards.interleave(tf.data.TFRecordDataset,
+                                cycle_length=num_shards,
+                                num_parallel_calls=tf.data.experimental.AUTOTUNE)
 
     dataset = dataset.shuffle(buffer_size=2048)
-    dataset = dataset.repeat()
     dataset = dataset.map(map_func=parser, num_parallel_calls=tf.data.experimental.AUTOTUNE)
     dataset = dataset.batch(batch_size)
     dataset = dataset.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)

data/yolact_parser.py
@@ -168,7 +168,7 @@ class Parser(object):
         image = data['image']
 
         # convert image to range [0, 1], faciliate augmentation
-        image = tf.image.convert_image_dtype(image, tf.float32)
+        image = normalize_image(image)
 
         # we already resize the image when creating tfrecord
         # image = tf.image.resize(image, [self._output_size, self._output_size])