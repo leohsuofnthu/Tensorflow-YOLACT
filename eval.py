8f95eb7761503a729caa19b92bd4bc776251912e
leohsuofnthu <leohsuofnthu@gmail.com>
2020-03-25 22:23:23 -0400

    starting evaluation part

---
+++ eval.py
@@ -0,0 +1,15 @@
+import os
+
+import tensorflow as tf
+
+from absl import app
+from absl import flags
+from absl import logging
+
+
+def main(argv):
+    pass
+
+
+if __name__ == '__main__':
+    app.run(main)
