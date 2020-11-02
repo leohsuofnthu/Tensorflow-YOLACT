2eb477b2a60a819999d5bc48a6829bae8dcfc850
leohsuofnthu <leohsuofnthu@gmail.com>
2020-11-02 00:18:26 -0500

    keep crowd annotation

--- data/coco_tfrecord_creator.py
+++ data/coco_tfrecord_creator.py
@@ -218,20 +218,12 @@ def _create_tf_record_from_coco_annotations(annotations_file, image_dir, output_
             if idx % 100 == 0:
                 logging.info('On image %d of %d', idx, len(images))
             annotations_list = annotations_index[image['id']]
-            # ignore image only have crowd annotation
-            num_crowd = 0
-            for object_annotations in annotations_list:
-                if object_annotations['iscrowd']:
-                    num_crowd += 1
-            if num_crowd != len(annotations_list):
-                _, tf_example, num_annotations_skipped = create_tf_example(
-                    image, annotations_list, image_dir, category_index, include_masks)
-                total_num_annotations_skipped += num_annotations_skipped
-                shard_idx = idx % num_shards
+            _, tf_example, num_annotations_skipped = create_tf_example(
+                image, annotations_list, image_dir, category_index, include_masks)
+            total_num_annotations_skipped += num_annotations_skipped
+            shard_idx = idx % num_shards
+            if tf_example:
                 output_tfrecords[shard_idx].write(tf_example.SerializeToString())
-            else:
-                logging.info('Image only have crowd annotation ignored')
-                total_num_annotations_skipped += len(annotations_list)
 
         logging.info('Finished writing, skipped %d annotations.',
                      total_num_annotations_skipped)
