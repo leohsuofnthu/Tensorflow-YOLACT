422375212740638fe5ce9211a649f6d398afe80f
leohsuofnthu <leohsuofnthu@gmail.com>
2020-11-01 21:44:29 -0500

    random sample crop finished, not tested

--- utils/augmentation.py
+++ utils/augmentation.py
@@ -11,6 +11,7 @@ Ref: https://github.com/dbolya/yolact/blob/821e83047847b9b1faf21b03b0d7ad521508f
 
 class Compose(object):
     """Composes several augmentations together."""
+
     def __init__(self, transforms):
         self.transforms = transforms
 
@@ -30,14 +31,6 @@ class Resize(object):
 
 # normalize image fot input
 class BackboneTransform(object):
-    def __init__(self):
-        ...
-
-    def __call__(self, image, masks=None, boxes=None, labels=None):
-        ...
-
-
-class RandomLightNoise(object):
     def __init__(self):
         ...
 
@@ -110,6 +103,7 @@ class PhotometricDistort(object):
 
     def __call__(self, image, masks, boxes, labels):
         image, masks, boxes, labels = self.rand_brightness(image, masks, boxes, labels)
+        # different order have different effect
         if tf.random.uniform([1]) > 0.5:
             photometric_distort = Compose(self.actions[:-1])
         else:
@@ -165,21 +159,58 @@ class Expand(object):
 
 class RandomSampleCrop(object):
     def __init__(self):
-        self.sample_options = {
-            # using entire original input image
-            None,
-            # sample a patch s.t. MIN jaccard w/ obj in .1,.3,.4,.7,.9
-            (0.1, None),
-            (0.3, None),
-            (0.7, None),
-            (0.9, None),
-            # randomly sample a patch
-            (None, None),
-        }
+        self.min_iou = [0, 0.1, 0.3, 0.7, 0.9, None]
 
     def __call__(self, image, masks, boxes=None, labels=None):
         # choose the min_object_covered value in self.sample_options
-        height, width, _ = image.get_shape()
+        idx = int(tf.random.uniform([1], minval=0, maxval=5.50))
+        min_iou = self.min_iou[idx]
+        if min_iou is None:
+            return image, masks, boxes, labels
+
+        # Geometric Distortions (img, bbox, mask)
+        bbox_begin, bbox_size, distort_bbox = tf.image.sample_distorted_bounding_box(
+            tf.shape(image),
+            bounding_boxes=tf.expand_dims(boxes, 0),
+            min_object_covered=min_iou,
+            aspect_ratio_range=(0.5, 2),
+            area_range=(0.1, 1.0),
+            max_attempts=50)
+
+        # the distort box is the area of the cropped image, original image will be [0, 0, 1, 1]
+        distort_bbox = distort_bbox[0, 0]
+
+        # cropped the image
+        cropped_image = tf.slice(image, bbox_begin, bbox_size)
+        cropped_image.set_shape([None, None, 3])
+
+        # cropped the mask
+        bbox_begin = tf.concat([[0], bbox_begin], axis=0)
+        bbox_size = tf.concat([[-1], bbox_size], axis=0)
+        cropped_masks = tf.slice(masks, bbox_begin, bbox_size)
+        cropped_masks.set_shape([None, None, None, 1])
+
+        # resize the scale of bboxes for cropped image
+        v = tf.stack([distort_bbox[0], distort_bbox[1], distort_bbox[0], distort_bbox[1]])
+        boxes = boxes - v
+        s = tf.stack([distort_bbox[2] - distort_bbox[0],
+                      distort_bbox[3] - distort_bbox[1],
+                      distort_bbox[2] - distort_bbox[0],
+                      distort_bbox[3] - distort_bbox[1]])
+        boxes = boxes / s
+
+        # filter out
+        scores = utils.bboxes_intersection(tf.constant([0, 0, 1, 1], boxes.dtype), boxes)
+        bool_mask = scores > 0.5
+        classes = tf.boolean_mask(labels, bool_mask)
+        bboxes = tf.boolean_mask(boxes, bool_mask)
+
+        # deal with negative value of bbox
+        bboxes = tf.clip_by_value(bboxes, clip_value_min=0, clip_value_max=1)
+
+        cropped_masks = tf.boolean_mask(cropped_masks, bool_mask)
+
+        return cropped_image, cropped_masks, bboxes, classes
 
 
 class RandomMirror(object):
@@ -239,48 +270,6 @@ def geometric_distortion(img, bboxes, masks, output_size, proto_output_size, cla
     cropped_image = tf.image.resize(cropped_image, [output_size, output_size], method=tf.image.ResizeMethod.BILINEAR)
 
     return cropped_image, bboxes, cropped_masks, classes
-
-
-"""Be Care for that, in here out input images have range [0, 1] instead of [0, 255]"""
-
-
-def photometric_distortion(image):
-    color_ordering = tf.random.uniform([1], minval=0, maxval=4)[0]
-
-    if color_ordering < 1 and color_ordering > 0:
-        image = tf.image.random_brightness(image, max_delta=0.12)
-        image = tf.image.random_saturation(image, lower=0.5, upper=1.5)
-        image = tf.image.random_hue(image, max_delta=0.2)
-        image = tf.image.random_contrast(image, lower=0.5, upper=1.5)
-
-    elif color_ordering < 2 and color_ordering > 1:
-        image = tf.image.random_saturation(image, lower=0.5, upper=1.5)
-        image = tf.image.random_brightness(image, max_delta=0.125)
-        image = tf.image.random_contrast(image, lower=0.5, upper=1.5)
-        image = tf.image.random_hue(image, max_delta=0.2)
-
-    elif color_ordering < 3 and color_ordering > 2:
-        image = tf.image.random_contrast(image, lower=0.5, upper=1.5)
-        image = tf.image.random_hue(image, max_delta=0.2)
-        image = tf.image.random_brightness(image, max_delta=0.125)
-        image = tf.image.random_saturation(image, lower=0.5, upper=1.5)
-
-    else:
-        image = tf.image.random_hue(image, max_delta=0.2)
-        image = tf.image.random_saturation(image, lower=0.5, upper=1.5)
-        image = tf.image.random_contrast(image, lower=0.5, upper=1.5)
-        image = tf.image.random_brightness(image, max_delta=0.125)
-
-    return image
-
-
-def horizontal_flip(image, bboxes, masks):
-    # Random mirroring (img, bbox, mask)
-    image = tf.image.flip_left_right(image)
-    masks = tf.image.flip_left_right(masks)
-    bboxes = tf.stack([bboxes[:, 0], 1 - bboxes[:, 3],
-                       bboxes[:, 2], 1 - bboxes[:, 1]], axis=-1)
-    return image, bboxes, masks
 
 
 def random_augmentation(img, bboxes, masks, output_size, proto_output_size, classes):
