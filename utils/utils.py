6e6f9fcb0184c9677899e56167a1eeca87c80de7
leohsuofnthu <leohsuofnthu@gmail.com>
2021-09-26 21:14:44 -0400

    remove redundant functions in utils

--- utils/utils.py
+++ utils/utils.py
@@ -7,6 +7,9 @@ https://github.com/dbolya/yolact/blob/master/layers/box_utils.py
 import tensorflow as tf
 
 
+# -----------------------------------------------------------------------------------------
+# Functions used by utils/augmentation.py
+
 def bboxes_intersection(bbox_ref, bboxes):
     """Compute relative intersection between a reference box and a
     collection of bounding boxes. Namely, compute the quotient between
@@ -44,51 +47,16 @@ def bboxes_intersection(bbox_ref, bboxes):
         tf.zeros_like(inter_vol), inter_vol / bboxes_vol)
 
 
-def normalize_image(image, offset=(0.407, 0.457, 0.485), scale=(0.225, 0.224, 0.229)):
-    """Normalizes the image to zero mean and unit variance."""
-    image = tf.image.convert_image_dtype(image, dtype=tf.float32)
-    offset = tf.constant(offset)
-    offset = tf.expand_dims(offset, axis=0)
-    offset = tf.expand_dims(offset, axis=0)
-    image -= offset
+# -----------------------------------------------------------------------------------------
+# Functions used by loss/loss_yolact.py
 
-    scale = tf.constant(scale)
-    scale = tf.expand_dims(scale, axis=0)
-    scale = tf.expand_dims(scale, axis=0)
-    image /= scale
-    return image
-
-
-def denormalize_image(image, offset=(0.407, 0.457, 0.485), scale=(0.225, 0.224, 0.229)):
-    """Normalizes the image to zero mean and unit variance."""
-    scale = tf.constant(scale)
-    scale = tf.expand_dims(scale, axis=0)
-    scale = tf.expand_dims(scale, axis=0)
-    image *= scale
-
-    offset = tf.constant(offset)
-    offset = tf.expand_dims(offset, axis=0)
-    offset = tf.expand_dims(offset, axis=0)
-    image += offset
-    return image
-
-
-# mapping from [ymin, xmin, ymax, xmax] to [cx, cy, w, h]
+# mapping from [xmin, ymin, xmax, ymax] to [cx, cy, w, h]
 def map_to_center_form(x):
     h = x[:, 2] - x[:, 0]
     w = x[:, 3] - x[:, 1]
     cy = x[:, 0] + (h / 2)
     cx = x[:, 1] + (w / 2)
     return tf.stack([cx, cy, w, h], axis=-1)
-
-
-# encode the gt and anchors to offset
-def map_to_offset(x):
-    g_hat_cx = (x[0, 0] - x[0, 1]) / x[2, 1]
-    g_hat_cy = (x[1, 0] - x[1, 1]) / x[3, 1]
-    g_hat_w = tf.math.log(x[2, 0] / x[2, 1])
-    g_hat_h = tf.math.log(x[3, 0] / x[3, 1])
-    return tf.stack([g_hat_cx, g_hat_cy, g_hat_w, g_hat_h])
 
 
 # crop the prediction of mask so as to calculate the linear combination mask loss
@@ -116,6 +84,9 @@ def crop(pred, boxes):
 
     return pred * crop_mask
 
+
+# -----------------------------------------------------------------------------------------
+# Functions used by layers/detection.py
 
 # decode the offset back to center form bounding box when evaluation and prediction
 def map_to_bbox(anchors, loc_pred):
@@ -148,6 +119,9 @@ def map_to_bbox(anchors, loc_pred):
     # tf.print("pred", tf.shape(loc_pred))
     return decoded_boxes
 
+
+# -----------------------------------------------------------------------------------------
+# Functions used by eval.py
 
 def intersection(box_a, box_b):
     # unstack the ymin, xmin, ymax, xmax
@@ -221,7 +195,7 @@ def mask_iou(masks_a, masks_b, is_crowd=False):
 
 def postprocess(detection, w, h, batch_idx, intepolation_mode="bilinear", crop_mask=True, score_threshold=0):
     """post process after detection layer"""
-    # Todo: If scorethreshold is not zero
+    # Todo: If score threshold is not zero
     """
     if score_threshold > 0:
         keep = detection['score'] > score_threshold
